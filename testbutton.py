a78a6b5d96a9e7719cc520109e890bec4168da72
haziqsyazwnnn13 <haziq.syazwnnn@gmail.com>
2025-11-02 19:49:00 +0800

    Update testbutton.py

--- testbutton.py
+++ testbutton.py
@@ -408,7 +408,7 @@ st.set_page_config(page_title="ONETICKET - Rock Indie ", layout="centered")
 
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2: 
-    st.header("ONE TICKET")
+    st.title("ONE TICKET")
 st.image("d1.png",width=1500)
 
 
@@ -545,6 +545,7 @@ st.markdown(
 
 
     
+
 
 
 
